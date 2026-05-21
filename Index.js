aee8685ff62d8bf684236f49a8be358657ce5475
Kamlakar Singh <kamlakar24169019@akgec.ac.in>
2026-05-21 15:45:28 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,26 @@
+function addTask() {
 
+    let taskInput = document.getElementById("taskInput");
+
+    let taskText = taskInput.value;
+
+    if (taskText === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+
+    li.innerHTML = `
+        ${taskText}
+        <button class="delete-btn">Delete</button>
+    `;
+
+    li.querySelector("button").addEventListener("click", function () {
+        li.remove();
+    });
+
+    document.getElementById("taskList").appendChild(li);
+
+    taskInput.value = "";
+}
